749ee9b0a51b31180e456d708372ccb8fe801568
Ebenezer-ebu <eifezulike@gmail.com>
2021-11-26 04:11:34 +0100

    adjustment

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("public"));
+// app.use(express.static("public"));
 app.use(cors());
 
 app.get("/root", (req, res) => {
