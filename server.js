2ae73d12c2714b3ac05a7baf7789ee7e26bf5128
Ebenezer-ebu <eifezulike@gmail.com>
2021-11-26 04:16:36 +0100

    adjustment

--- server.js
+++ server.js
@@ -115,19 +115,19 @@ app.get("/root", (req, res) => {
   res.send(help);
 });
 
-app.use((req, res, next) => {
-  const token = req.get("Authorization");
+// app.use((req, res, next) => {
+//   const token = req.get("Authorization");
 
-  if (token) {
-    req.token = token;
-    next();
-  } else {
-    res.status(403).send({
-      error:
-        "Please provide an Authorization header to identify yourself (can be whatever you want)",
-    });
-  }
-});
+//   if (token) {
+//     req.token = token;
+//     next();
+//   } else {
+//     res.status(403).send({
+//       error:
+//         "Please provide an Authorization header to identify yourself (can be whatever you want)",
+//     });
+//   }
+// });
 
 app.get("/categories", (req, res) => {
   categories.getAll(req.token).then(
